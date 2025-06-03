8afe6e08c6893d61d4842cfee498659484153d66
Gustavo Caqui <gustavocaqui94@gmail.com>
2025-06-03 00:18:03 -0500

    add interceptor with file

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppConfigService } from './app-config/app-config.service';
 import { ValidationPipe } from '@nestjs/common';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -11,6 +12,10 @@ async function bootstrap() {
     // Usando el método de conveniencia para obtener la configuración del servidor
     const { port, host } = config.getServerConfig();
     const { name } = config.getEnvName();
+
+    // Aumentar el límite de tamaño para archivos grandes si es necesario
+    app.use(json({ limit: '50mb' }));
+    app.use(urlencoded({ extended: true, limit: '50mb' }));
 
     app.setGlobalPrefix('api/v1');
     app.enableCors({
--- src/math-solve/controllers/math-solve.controller.ts
+++ src/math-solve/controllers/math-solve.controller.ts
@@ -1,13 +1,47 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { MathSolveService } from '../services/math-solve.service';
 import { MathSolveDto } from '../dtos/math-solve.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import * as multer from 'multer';
+import { join } from 'path';
 
 @Controller('math-solve')
 export class MathSolveController {
     constructor(private readonly mathSolveService: MathSolveService) {}
 
+    @UseInterceptors(
+        FileInterceptor('file', {
+            storage: multer.diskStorage({
+                destination: join(`${process.cwd()}/uploads`),
+                filename: (req, file, cb) => {
+                    cb(null, Date.now() + '-' + file.originalname);
+                },
+            }),
+            limits: {
+                fileSize: 1024 * 1024 * 5,
+            },
+            fileFilter: (req, file, callback) => {
+                if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+                    return callback(new Error('Only image files are allowed!'), false);
+                }
+                callback(null, true);
+            },
+        }),
+    )
     @Post('chat')
-    solveMathProblem(@Body() body: MathSolveDto) {
-        return this.mathSolveService.solveMathProblem(body.prompt);
+    solveMathProblem(@UploadedFile() file: Express.Multer.File, @Body() body: MathSolveDto) {
+        console.log({ body, file });
+        return {
+            message: 'Math problem solved',
+            body,
+            file: {
+                fileName: file.filename,
+                originalName: file.originalname,
+                size: file.size,
+                encoding: file.encoding,
+                mimetype: file.mimetype,
+                path: file.path,
+            },
+        };
     }
 }
